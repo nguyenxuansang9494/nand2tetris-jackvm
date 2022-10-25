f24a8538a64b77406272991365d696d63a1f4e88
Sang Nguyen <nguysa02@risk.regn.net>
2022-10-25 13:15:08 +0700

    optimize

--- src/arithmetic_translator.go
+++ src/arithmetic_translator.go
@@ -4,10 +4,26 @@ func TranslateArithmeticStatement(stmt ArithmeticStatement) []string {
 	return nil
 }
 
-func translateUnaryOperation() []string {
-	return nil
+func translateUnaryOperation(stmt ArithmeticStatement) []string {
+	rs := make([]string, 3)
+	if stmt.Command == "not" {
+		rs = append(rs, "@SP")
+		rs = append(rs, "A=M-1")
+		rs = append(rs, "M=!M")
+	} else if stmt.Command == "neg" {
+		rs = append(rs, "@SP")
+		rs = append(rs, "A=M-1")
+		rs = append(rs, "M=-M")
+	}
+	return rs
 }
 
-func translateBinaryOperation() []string {
+func translateBinaryOperation(stmt ArithmeticStatement) []string {
+	rs := make([]string, 6)
+	rs = append(rs, "@SP")
+	rs = append(rs, "AM=M-1")
+	rs = append(rs, "D=M")
+	rs = append(rs, "@SP")
+	rs = append(rs, "A=M-1")
 	return nil
 }
